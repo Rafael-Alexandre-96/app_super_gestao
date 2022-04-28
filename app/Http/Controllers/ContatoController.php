1ca057dfc8711ec34126038c1a5b3cba70ed590a
Rafael Alexandre <rafael.alexandre.1996.ra@gmail.com>
2022-04-27 22:36:04 -0300

    Desenvolvendo FORMs

--- app/Http/Controllers/ContatoController.php
+++ app/Http/Controllers/ContatoController.php
@@ -2,11 +2,26 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\SiteContato;
 use Illuminate\Http\Request;
 
 class ContatoController extends Controller
 {
-    public function contato(){
+    public function contato(Request $request){
+        $contato = new SiteContato();
+
+        /*
+        $contato->nome = $request->input('nome');
+        $contato->telefone = $request->input('telefone');
+        $contato->email = $request->input('email');
+        $contato->motivo_contato = $request->input('motivo_contato');
+        $contato->mensagem = $request->input('mensagem');
+
+        $contato->save();
+        */
+
+        $contato->create($request->all());
+
         return view('site.contato');
     }
 }
